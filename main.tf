31f4b78768ab5cb6added1cca6d07a5d368b10cf
Kazuto Kusama <jacopen@gmail.com>
2022-03-15 14:48:31 +0000

    Update .tf

--- main.tf
+++ main.tf
@@ -50,6 +50,20 @@ resource "azurerm_network_security_rule" "atlantis" {
   network_security_group_name = module.linuxservers.network_security_group_name
 }
 
+resource "azurerm_network_security_rule" "http" {
+  name                        = "atlantis"
+  priority                    = 104
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "80"
+  source_address_prefix       = "0.0.0.0/0"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.wellsjp.name
+  network_security_group_name = module.linuxservers.network_security_group_name
+}
+
 output "linux_vm_public_name" {
   value = module.linuxservers.public_ip_dns_name
 }
